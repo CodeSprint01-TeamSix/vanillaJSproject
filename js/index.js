f82c091aa25b6bbc470bb0c545c68b0889280709
namminimi <tbvhdaos@naver.com>
2023-10-19 00:50:33 +0900

    feat(member):글자 형태 변경

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
-
+/* 리셋 클릭 했을 때 글자 생성 */
 const handleClick = async () => {
     const missionElement = $('.mission');
     missionElement.innerHTML = "";
@@ -19,8 +19,11 @@ const handleClick = async () => {
     mission.split("").map(word => {
         const divElement = document.createElement('div')
         divElement.classList.add('word');
-        divElement.innerHTML = `<h3>${word}</h3><span></span>`;
-        missionElement.append(divElement)
+        
+        const replaceStar = word.replace(word, '?') /* 출력문자 물음표로 변경 */
+
+        divElement.innerHTML = `<h3>${replaceStar}</h3><span></span>`;
+        missionElement.append(divElement);
     })
 
 }
