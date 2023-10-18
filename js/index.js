cfa46b6cb9c47b51938dc2fd0b0182e4479f49cd
namminimi <tbvhdaos@naver.com>
2023-10-19 01:05:14 +0900

    refector(member):fetch함수와 글자 출력함수 리팩토리

--- js/index.js
+++ js/index.js
@@ -3,24 +3,30 @@ function $(selector) {
     return document.querySelector(selector);
 }
 
+const createJsonWords = async () => {
+    const response = await fetch('../world.json');
+    const result = await response.json();
+    const {practice} = result;
+
+    const number = Math.floor(Math.random() * practice.length);
+    const {mission} = practice[number]
+    return mission;
+}
+
+
+
 /* 리셋 클릭 했을 때 글자 생성 */
 const handleClick = async () => {
     const missionElement = $('.mission');
     missionElement.innerHTML = "";
 
-    const response = await fetch('../world.json');
-    const result = await response.json();
-    const {practice} = result;
-
-    
-    const number = Math.floor(Math.random() * practice.length);
-    const {mission} = practice[number]
+    const mission = await createJsonWords();
 
     mission.split("").map(word => {
         const divElement = document.createElement('div')
         divElement.classList.add('word');
-        
-        const replaceStar = word.replace(word, '?') /* 출력문자 물음표로 변경 */
+        /* 출력문자 물음표로 변경 */
+        const replaceStar = word !== " " ? word.replace(word, '?') : word.replace(word, ' ') 
 
         divElement.innerHTML = `<h3>${replaceStar}</h3><span></span>`;
         missionElement.append(divElement);
@@ -28,6 +34,7 @@ const handleClick = async () => {
 
 }
 
+handleClick();
 
 $('button').addEventListener('click', handleClick);
 
