15f2e5f1ea2d0be581035e72419f6ac82d2d52da
namminimi <tbvhdaos@naver.com>
2023-10-19 00:44:10 +0900

    feat(member): 글자 생성 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+
+const handleClick = async () => {
+    const missionElement = $('.mission');
+    missionElement.innerHTML = "";
+
+    const response = await fetch('../world.json');
+    const result = await response.json();
+    const {practice} = result;
+
+    
+    const number = Math.floor(Math.random() * practice.length);
+    const {mission} = practice[number]
+
+    mission.split("").map(word => {
+        const divElement = document.createElement('div')
+        divElement.classList.add('word');
+        divElement.innerHTML = `<h3>${word}</h3><span></span>`;
+        missionElement.append(divElement)
+    })
+
+}
+
+
+$('button').addEventListener('click', handleClick);
+
